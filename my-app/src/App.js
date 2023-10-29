6b756eff32a71ccbcc27256fd1c93ac2260e0c2a
[Muhammad Haseeb] <[haseeb241202@gmail.com]>
2023-10-29 03:42:20 -0700

    my first project host on github

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+// import TodoForm from './components/TodoForm';
+import Todolist from './components/Todolist';
+// import {RiCloseCircleLine} from 'react-icons/ri'
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      {/* <TodoForm/> */}
+      <Todolist/>
+      {/* <RiCloseCircleLine/> */}
+
+     
+    </div>
+  );
+}
+
+export default App;
